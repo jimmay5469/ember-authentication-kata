a5bd7fc26108d89f052b2280eb8918099fd6404b
jimmay5469 <jimmay5469@gmail.com>
2015-12-16 13:59:54 -0500

    Display links

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,4 +1,5 @@
 export default function() {
+  this.get('/links');
 
   // These comments are here to help you get started. Feel free to delete them.
 
---
+++ tests/unit/routes/add-link-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:add-link', 'Unit | Route | add link', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
