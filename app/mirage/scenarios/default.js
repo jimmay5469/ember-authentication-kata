998b7be9fa22720029f859fabb4d919a5b53ce50
jimmay5469 <jimmay5469@gmail.com>
2015-12-16 15:37:43 -0500

    Remove commenting from ember-cli-mirage files

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -1,8 +1,3 @@
 export default function(server) {
-
-  // Seed your development database using your factories. This
-  // data will not be loaded in your tests.
-
-  // server.createList('contact', 10);
   server.createList('link', 10);
 }
