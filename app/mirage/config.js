0322ed2c77cdbe391227c36e5a2a669f4c5bd221
jimmay5469 <jimmay5469@gmail.com>
2015-12-16 15:36:56 -0500

    Require auth token when adding links

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -2,8 +2,19 @@ import Mirage from 'ember-cli-mirage';
 
 export default function() {
   this.get('/links');
-  this.post('/links');
-  this.post('/token', function(server, request) {
+  this.post('/links', function(db, request) {
+    if(request.requestHeaders.Authorization === 'Bearer abc123') {
+      var data = JSON.parse(request.requestBody).link;
+      return { link: db.links.insert(data) };
+    } else {
+      return new Mirage.Response(401, {Status: '401 Unauthorized'}, {
+        error: 'invalid_grant',
+        error_description: 'The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.'
+      });
+    }
+  });
+  this.post('/token', function(db, request) {
+    console.log(request);
     if(request.requestBody === 'grant_type=password&username=test&password=pass') {
       return {
         access_token: 'abc123',
@@ -15,7 +26,8 @@ export default function() {
     } else {
       return new Mirage.Response(401, {Status: '401 Unauthorized'}, {
         error: 'invalid_grant',
-        error_description: 'The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.'});
+        error_description: 'The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.'
+      });
     }
   });
 
