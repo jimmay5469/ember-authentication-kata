518b9a63798b79d2a14672d4e31dacb914e66f4d
jimmay5469 <jimmay5469@gmail.com>
2015-12-16 14:20:06 -0500

    Allow adding links

---
+++ app/controllers/add-link.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  title: 'test',
+  url: 'http://google.com',
+  description: 'this is just a test'
+});
--- app/routes/add-link.js
+++ app/routes/add-link.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addLink() {
+      this.store.createRecord('link', {
+        title: this.get('controller.title'),
+        url: this.get('controller.url'),
+        description: this.get('controller.description')
+      }).save();
+      this.transitionTo('links');
+    }
+  }
 });
