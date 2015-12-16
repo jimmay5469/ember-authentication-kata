4f34cf0c41bea074c463cad39a623e6440c61432
jimmay5469 <jimmay5469@gmail.com>
2015-12-16 15:55:17 -0500

    Cleanup mirage files for kata gist

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,33 +1,40 @@
 import Mirage from 'ember-cli-mirage';
 
+let USERNAME = 'test';
+let PASSWORD = 'pass';
+let TOKEN = 'abc123';
+
+function getUnauthorizedResponse() {
+  return new Mirage.Response(401, {Status: '401 Unauthorized'}, {
+    error: 'invalid_grant',
+    error_description: 'The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.'
+  });
+}
+function getTokenResponse() {
+  return {
+    access_token: TOKEN,
+    token_type: 'bearer',
+    expires_in: 7200,
+    refresh_token: 'zyz789',
+    created_at: new Date().getTime()
+  };
+}
+
 export default function() {
   this.get('/links');
   this.post('/links', function(db, request) {
-    if(request.requestHeaders.Authorization === 'Bearer abc123') {
+    if(request.requestHeaders.Authorization === `Bearer ${TOKEN}`) {
       var data = JSON.parse(request.requestBody).link;
       return { link: db.links.insert(data) };
     } else {
-      return new Mirage.Response(401, {Status: '401 Unauthorized'}, {
-        error: 'invalid_grant',
-        error_description: 'The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.'
-      });
+      return getUnauthorizedResponse();
     }
   });
   this.post('/token', function(db, request) {
-    console.log(request);
-    if(request.requestBody === 'grant_type=password&username=test&password=pass') {
-      return {
-        access_token: 'abc123',
-        token_type: 'bearer',
-        expires_in: 7200,
-        refresh_token: 'zyz789',
-        created_at: new Date().getTime()
-      }
+    if(request.requestBody === `grant_type=password&username=${USERNAME}&password=${PASSWORD}`) {
+      return getTokenResponse();
     } else {
-      return new Mirage.Response(401, {Status: '401 Unauthorized'}, {
-        error: 'invalid_grant',
-        error_description: 'The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.'
-      });
+      return getUnauthorizedResponse();
     }
   });
 }
